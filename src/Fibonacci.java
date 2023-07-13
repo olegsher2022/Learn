01bfc6f526deb7e064e4bd3a3580f5a68de802ed
Oleg Sher <olegsher@revuze.it>
2023-07-13 12:45:51 +0300

    Fibonacci

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,4 +1,5 @@
 public class Fibonacci {
+    private static int[] memo;
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
@@ -6,5 +7,42 @@ public class Fibonacci {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
+
+    public static int fibonacci_1(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int fib = 1;
+        int prevFib = 1;
+
+        for (int i = 2; i < n; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+
+        return fib;
+    }
+
+
+    public static int fibonacci_2(int n) {
+        memo = new int[n + 1];
+        return fibonacciHelper(n);
+    }
+
+    private static int fibonacciHelper(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        memo[n] = fibonacciHelper(n - 1) + fibonacciHelper(n - 2);
+        return memo[n];
+    }
+
 }
 
--- src/FibonacciTest.java
+++ src/FibonacciTest.java
@@ -10,6 +10,20 @@ public class FibonacciTest {
         assertEquals(2, Fibonacci.fibonacci(3));
         assertEquals(3, Fibonacci.fibonacci(4));
         assertEquals(5, Fibonacci.fibonacci(5));
+
+        assertEquals(0, Fibonacci.fibonacci_1(0));
+        assertEquals(1, Fibonacci.fibonacci_1(1));
+        assertEquals(1, Fibonacci.fibonacci_1(2));
+        assertEquals(2, Fibonacci.fibonacci_1(3));
+        assertEquals(3, Fibonacci.fibonacci_1(4));
+        assertEquals(5, Fibonacci.fibonacci_1(5));
+
+        assertEquals(0, Fibonacci.fibonacci_2(0));
+        assertEquals(1, Fibonacci.fibonacci_2(1));
+        assertEquals(1, Fibonacci.fibonacci_2(2));
+        assertEquals(2, Fibonacci.fibonacci_2(3));
+        assertEquals(3, Fibonacci.fibonacci_2(4));
+        assertEquals(5, Fibonacci.fibonacci_2(5));
         // Add more test cases as needed
     }
 }
